11321fadbed595ea7c34c41f7984e121818a5d1f
montilloo <nikun2008@126.com>
2021-11-17 20:38:12 +0800

    Configuring & Styling a TextInput;

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Button, Text, TextInput, View, StyleSheet} from "react-native";
+import {Button, Text, View, StyleSheet} from "react-native";
 import Card from "../components/Card";
+import Input from "../components/Input";
 import Colors from "../constants/colors";
 
 const StartGameScreen = () => {
@@ -10,7 +11,7 @@ const StartGameScreen = () => {
 			<Card style={styles.inputContainer}>
 				<View style={styles.inputContainer}>
 					<Text>Select a Number</Text>
-					<TextInput/>
+					<Input style={styles.input} blurOnSubmit autoCapitalize="none" autoCorrent={false} keyboardType='number-pad'/>
 					<View style={styles.buttonContainer}>
 						<View style={styles.button}><Button title='RESET' onPress={() => {
 						}} color={Colors.accent}/></View>
@@ -33,14 +34,12 @@ const styles = StyleSheet.create(
 			title: {
 				fontSize: 20,
 				marginVertical: 10
-			}
-			,
+			},
 			inputContainer: {
 				width: 300,
 				maxWidth: '80%',
 				alignItems: 'center',
-			}
-			,
+			},
 			buttonContainer: {
 				flexDirection: 'row',
 				width: '100%',
@@ -48,7 +47,11 @@ const styles = StyleSheet.create(
 				justifyContent: "space-between"
 			},
 			button: {
-
+				width: 100
+			},
+			input: {
+				width: 50,
+				textAlign: "center"
 			}
 		}
 	)
