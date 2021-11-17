e6edc7d5cd98c4c866d4cfd2a80d928daf3b7cc9
montilloo <nikun2008@126.com>
2021-11-17 19:55:48 +0800

    Extracting a Card Component(Presentational Component);

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,20 +1,23 @@
 import React from "react";
 import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import Card from "../components/Card";
 
 const StartGameScreen = () => {
 	return (
 		<View style={styles.screen}>
 			<Text style={styles.title}>Start a New Game!</Text>
-			<View style={styles.inputContainer}>
-				<Text>Select a Number</Text>
-				<TextInput/>
-				<View style={styles.buttonContainer}>
-					<Button title='Reset' onPress={() => {
-					}}/>
-					<Button title='Confirm' onPress={() => {
-					}}/>
+			<Card style={styles.inputContainer}>
+				<View style={styles.inputContainer}>
+					<Text>Select a Number</Text>
+					<TextInput/>
+					<View style={styles.buttonContainer}>
+						<Button title='Reset' onPress={() => {
+						}}/>
+						<Button title='Confirm' onPress={() => {
+						}}/>
+					</View>
 				</View>
-			</View>
+			</Card>
 		</View>
 	);
 }
@@ -33,17 +36,6 @@ const styles = StyleSheet.create({
 		width: 300,
 		maxWidth: '80%',
 		alignItems: 'center',
-		shadowColor: 'black',
-		shadowOffset: {
-			width: 0,
-			height: 2
-		},
-		shadowRadius: 6,
-		shadowOpacity: 0.26,
-		backgroundColor: 'white',
-		elevation: 8,
-		padding: 20,
-		borderRadius: 10
 	},
 	buttonContainer: {
 		flexDirection: 'row',
