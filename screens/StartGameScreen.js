9f1b32ecc25dfa164c6bfdda29587ac25b247e18
montilloo <nikun2008@126.com>
2021-11-17 20:15:01 +0800

    Color Theming with Constants;

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
-import {Button, StyleSheet, Text, TextInput, View} from "react-native";
+import {Button, Text, TextInput, View, StyleSheet} from "react-native";
 import Card from "../components/Card";
+import Colors from "../constants/colors";
 
 const StartGameScreen = () => {
 	return (
@@ -11,10 +12,10 @@ const StartGameScreen = () => {
 					<Text>Select a Number</Text>
 					<TextInput/>
 					<View style={styles.buttonContainer}>
-						<Button title='Reset' onPress={() => {
-						}}/>
-						<Button title='Confirm' onPress={() => {
-						}}/>
+						<View style={styles.button}><Button title='RESET' onPress={() => {
+						}} color={Colors.accent}/></View>
+						<View style={styles.button}><Button title='CONFIRM' onPress={() => {
+						}} color={Colors.primary}/></View>
 					</View>
 				</View>
 			</Card>
@@ -22,27 +23,35 @@ const StartGameScreen = () => {
 	);
 }
 
-const styles = StyleSheet.create({
-	screen: {
-		flex: 1,
-		padding: 10,
-		alignItems: 'center',
-	},
-	title: {
-		fontSize: 20,
-		marginVertical: 10
-	},
-	inputContainer: {
-		width: 300,
-		maxWidth: '80%',
-		alignItems: 'center',
-	},
-	buttonContainer: {
-		flexDirection: 'row',
-		width: '100%',
-		paddingHorizontal: 15,
-		justifyContent: "space-between"
-	}
-});
+const styles = StyleSheet.create(
+		{
+			screen: {
+				flex: 1,
+				padding: 10,
+				alignItems: 'center',
+			},
+			title: {
+				fontSize: 20,
+				marginVertical: 10
+			}
+			,
+			inputContainer: {
+				width: 300,
+				maxWidth: '80%',
+				alignItems: 'center',
+			}
+			,
+			buttonContainer: {
+				flexDirection: 'row',
+				width: '100%',
+				paddingHorizontal: 15,
+				justifyContent: "space-between"
+			},
+			button: {
+
+			}
+		}
+	)
+;
 
 export default StartGameScreen;
