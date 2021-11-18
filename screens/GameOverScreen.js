542df824c05308813abf977c0ec24ffe6c9b8309
montilloo <nikun2008@126.com>
2021-11-18 16:49:32 +0800

    Styling Images;

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -6,7 +6,9 @@ import DefaultStyles from "../constants/default-styles";
 const GameOverScreen = props => {
 	return <View style={styles.screen}>
 		<Text style={{ ...DefaultStyles.title }}>The Game is Over!</Text>
-		<Image source={ require("../assets/success.png")} />
+		<View style={styles.imageContainer}>
+			<Image source={ require("../assets/success.png")} style={styles.image} resizeMode="cover" />
+		</View>
 		<Text style={{ ...DefaultStyles.bodyText }}>Rounds Number: { props.roundsNumber }</Text>
 		<Text style={{ ...DefaultStyles.bodyText }}>User Number: { props.userNumber }</Text>
 		<Button title="NEW GAME" onPress={ props.onRestart} />
@@ -18,6 +20,19 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: "center",
 		alignItems: "center"
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: 'black',
+		marginVertical: 30,
+		overflow: "hidden"
+	},
+	image: {
+		width: '100%',
+		height: '100%'
 	}
 });
 
