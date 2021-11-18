37f3e603737f8a9076320891aa3bf7b56215628a
montilloo <nikun2008@126.com>
2021-11-18 16:38:37 +0800

    Adding Local Images;

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet, Button} from "react-native";
+import { View, Text, StyleSheet, Button, Image } from "react-native";
+
+import DefaultStyles from "../constants/default-styles";
 
 const GameOverScreen = props => {
 	return <View style={styles.screen}>
-		<Text>The Game is Over!</Text>
-		<Text>Rounds Number: { props.roundsNumber }</Text>
-		<Text>User Number: { props.userNumber }</Text>
+		<Text style={{ ...DefaultStyles.title }}>The Game is Over!</Text>
+		<Image source={ require("../assets/success.png")} />
+		<Text style={{ ...DefaultStyles.bodyText }}>Rounds Number: { props.roundsNumber }</Text>
+		<Text style={{ ...DefaultStyles.bodyText }}>User Number: { props.userNumber }</Text>
 		<Button title="NEW GAME" onPress={ props.onRestart} />
 	</View>
 };
